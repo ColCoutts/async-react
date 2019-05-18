9eebbe2de6e654be2d7d1ff3c78dd1d6f503a838
COLIN COUTTS <COUTTSTCOLIN@GMAIL.COM>
2019-05-18 11:31:32 -0700

    paging functionality built

--- src/components/App.js
+++ src/components/App.js
@@ -2,5 +2,5 @@ import React from 'react';
 import AllCharacters from '../containers/quotes/AllCharacters';
 
 export default function App() {
-  return <AllCharacters />;
+  return <AllCharacters page={1} />;
 }
